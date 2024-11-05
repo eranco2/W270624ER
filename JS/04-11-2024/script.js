25e2133bb5020613a4daa34fc828d4706cc953c7
eranco <eycohen23@gmail.com>
2024-11-05 14:03:49 +0200

    first commit

---
+++ JS/04-11-2024/script.js
@@ -0,0 +1,77 @@
+// const user = 'yoni';
+// const pass = '1234';
+
+// const user1 = prompt("please enter your username:");
+// const pass1 = prompt("please enter your password:");
+
+// user1 === user && pass1 === pass && console.log("welcome " + user1);
+// user1 !== user && pass1 !== pass && console.log("wrong !");
+
+// const number1 = +prompt("please enter a number1:");
+// const number2 = +prompt("please enter a number2:");
+
+// number1 > number2 && console.log(number1 + " is bigger then " + number2);
+// number1 < number2 && console.log(number1 + " is smaller then " + number2);
+// number1 === number2 && console.log(number1 + " is equal to " + number2);
+
+// /////////////////////////////////////////////  conditions  //////////////////////////////////////////////
+
+
+// function compareNumber(num3, num4) {
+//     if (num3 > num4) {
+//         console.log(num3 + " is bigger than " + num4);
+//     }
+//     // if (num3 < num4) {
+//     else if (num3 < num4) {
+//         console.log(num3 + " is smaller than " + num4);
+//     } else if (isNaN(num3) && isNaN(num4)) {
+//         console.log("please enter a number");
+//     }
+//     // if (num3 === num4) {
+//     else {
+//         console.log(num3 + " is equal to " + num4);
+//     }
+// }
+
+// const number3 = +prompt("please enter a number3:");
+// const number4 = +prompt("please enter a number4:");
+// compareNumber(number3, number4);
+
+// compareNumber(1, 3);
+
+
+/////////////////////////////// targil 1 //////////////////////////////////////////
+
+// const username = prompt("please enter your name:");
+// const days = +prompt("hello " + username + " please enter the number for your visit");
+// const pricePerDay = 100;
+// const pricePerMeel = 30;
+// let total = days * pricePerDay;
+
+// // console.log(username + " you will stay for " + days + " days and the total price is " + total + "$");
+// const breakfast = prompt("do you want breakfast for " + days + " days? (yes/no)");
+// if (breakfast === "yes") {
+//     total += days * pricePerMeel;
+// } else if (breakfast === "no") {
+//     total
+// }
+// else {
+//     console.log("please try again and choose yes on no");
+// }
+
+// console.log(username + " you will stay for " + days + " days and the total price is " + total + "$");
+
+//////////////////////////////// targil 2 /////////////////////////////////////////
+
+const uName = prompt("Hey ! please enter your name :");
+const perPepole = prompt(uName + "how many gusse you will be?");
+if (perPepole <= 2) {
+    const M = alert("room M cost 200$ fer person");
+} else if (perPepole < 5 < 2) {
+    const L = alert("room L cost 100$ fer person");
+} else (perPepole > 5 < 15)
+const XL = alert("room XL cost 50$ fer person");
+
+const days = +prompt("how long you stay?")
+
+total = days * perPepole 
